481c7d20607392228cc458ebdb9547b4a13f0f46
salcarazp <62428585+salcarazp@users.noreply.github.com>
2020-03-21 11:50:02 +0100

    Create readme.py

---
+++ readme.py
@@ -0,0 +1,9 @@
+
+# Este comentario aclara el tipo de codificación del texto
+# -*- coding: utf-8 -*-
+"""
+Este es el primer archivo del proyecto donde vamos a ir describiendo el objetivo y tipo de proyecto.
+Funcionará como una declaración de intenciones y una guía para comenzar el proyecto.
+(Notese que los comentarios de varias lineas en Python se generan con 3 veces las " al principio y al final del mismo)
+
+"""
